81080a6c24c6ce1f66f0d26a102f4b55706759d4
Rudy Rigot <rudy.rigot@salesforce.com>
2025-02-23 21:14:39 -0600

    First time listing all static weeks

--- src/build.rb
+++ src/build.rb
@@ -1,3 +1,45 @@
-html = File.read('src/index.html')
+require 'json'
+require 'date'
 
-File.write('index.html', html)
+html = File.read('src/index.html')
+html_my_weeks = StringIO.new
+
+json = JSON.parse(File.read('src/weeks.json'))
+
+events = {}
+
+json['categories'].each do | category_name, category_content |
+    category_symbol = category_content['symbol']
+    category_content['events'].each do | date, texts |
+        shorttext = texts['shorttext']
+        longtext = texts['longtext']
+        raise "The date #{date} is appearing twice, can't do that." if events.key?(date)
+        events[date] = {
+            category_symbol: category_symbol,
+            shorttext: shorttext,
+            longtext: longtext
+        }
+    end
+end
+
+puts JSON.pretty_generate(events)
+
+last_date = Date.parse(events.keys.max)
+puts "Last date: #{last_date}"
+
+week_start_date = Date.parse(json['birth']['date'])
+
+while week_start_date <= last_date
+    week_end_date = week_start_date + 6
+
+    html_my_weeks << '<span class="week">'
+    html_my_weeks << 'From '
+    html_my_weeks << week_start_date
+    html_my_weeks << ' to '
+    html_my_weeks << week_end_date
+    html_my_weeks << '</span>'
+
+    week_start_date += 7
+end
+
+File.write('index.html', html.gsub('{{my_weeks}}', html_my_weeks.string))
